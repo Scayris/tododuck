0a69c0e41874f1dc0a863cdefd92888e674f3217
Nishale <jan.makovecki@gmail.com>
2015-11-22 21:14:43 +0100

    Script removing, completing tasks

--- scripts/tododuck.js
+++ scripts/tododuck.js
@@ -2,12 +2,19 @@
 var adding = false;		//is a task being edited or added?
 var username = "User";
 
-var tasks = [];			//array holding all the tasks.
-var shown = [];			//array holding the currently displayed tasks.
+var tasks = [];			//array holding all tasks
+var shown = [];			//array holding all incomplete tasks
+var completed = [];		//array holding completed tasks
+
+var taskNum = tasks.length;
 
 var defaultPriority = 3;
+var maxPriority = 5;
 
-var clickedTask;
+var colorNotImportant = "#009688";
+var colorSemiImportant = "#827717";
+var colorVeryImportant = "#F57C00";
+var colorUrgent = "#E53935";
 
 /*	On document ready
  *
@@ -40,6 +47,13 @@ $(document).ready(function () {	//ready the page
 		$("#task-editor > .mdl-card__title > .mdl-card__title-text").text("Add task");
 		$("#task-editor").fadeIn(200);
 		$("#task-editor-overlay").fadeIn(200);
+		
+		editor.find("#editing-index").val("");
+		editor.find("#task-text").val("");
+		editor.find("#place").val("");
+		editor.find("#deadline").val("");
+		editor.find("#priority-slider").attr("max",maxPriority);
+		editor.find("#priority-slider").get(0).MaterialSlider.change(defaultPriority);
 	});
 	
 	//task editor
@@ -66,14 +80,25 @@ $(document).ready(function () {	//ready the page
  */
 function newTask() {
 	return {
+		origId:"",
 		text:"",
 		priority:defaultPriority,
 		place:"",
 		deadline:"",
 		category:"",
-		done:0,
+		done:false,
 		domPtr:null
 	};
+}
+
+/*	sortTasks()
+ * 
+ * Sort tasks by priority
+ */
+function sortTasks() {
+	shown.sort(function(a, b) {
+		return b.priority - a.priority;
+	});
 }
 
 /*	showTasks()
@@ -81,6 +106,8 @@ function newTask() {
  * Generate DOM objects from tasks list
  */
 function showTasks() {
+	sortTasks();
+	
 	var template = $("#empty-task");
 	
 	$("#task-list").empty().append(template);
@@ -92,13 +119,32 @@ function showTasks() {
 		domObj = template.clone();
 		current = shown[i];
 		
-		var j = i;
-		
 		domObj.attr("id",i);
 		domObj.find(".task-content").text(current.text);
 		domObj.find(".urgency-holder").text(current.priority);
+		domObj.find(".date-holder").text(current.deadline);
 		domObj.find("#maps-link").attr("href","http://maps.google.com/?q="+current.place);
 		domObj.find("#editor-link").on("click", { id:i }, function(event) { editTask(event.data.id); });
+		domObj.find("#remove-link").on("click", { id:i }, function(event) { removeTask(event.data.id); });
+		domObj.find("#complete-link").on("click", { id:i }, function(event) { completeTask(event.data.id); });
+		
+		// set deadline warning
+		if (current.deadline == "") {
+			domObj.find(".date-warning").hide();
+		} else {
+			domObj.find(".date-warning").show();
+		}
+		
+		// color the priority holder
+		if (current.priority >= maxPriority) {
+			domObj.find(".urgency-holder").css("background-color", colorUrgent);
+		} else if (current.priority > maxPriority * (2/3)) {
+			domObj.find(".urgency-holder").css("background-color", colorVeryImportant);
+		} else if (current.priority > maxPriority / 3) {
+			domObj.find(".urgency-holder").css("background-color", colorSemiImportant);
+		} else {
+			domObj.find(".urgency-holder").css("background-color", colorNotImportant);
+		}
 		
 		//make task visible
 		domObj.css("display","block");
@@ -118,13 +164,41 @@ function editTask(id) {
 	$("#task-editor-overlay").fadeIn(200);
 	
 	var editor = $("#task-editor");
-	var task = tasks[id];
+	var task = shown[id];
 	
+	editor.find("#editing-index").val(id);
 	editor.find("#task-text").val(task.text).parent().addClass("is-dirty");			//mark as dirty to remove helper text
 	editor.find("#place").val(task.place).parent().addClass("is-dirty");			//mark as dirty to remove helper text
 	editor.find("#deadline").val(task.deadline).parent().addClass("is-dirty");		//mark as dirty to remove helper text
+	editor.find("#priority-slider").attr("max",maxPriority);
 	editor.find("#priority-slider").get(0).MaterialSlider.change(task.priority);	//needs HTML element to call change method
 	
+}
+
+/*	removeTask()
+ * 
+ */
+function removeTask(id) {
+	var removing = shown.splice(id, 1);
+	
+	for (var i in tasks) {
+		if (removing.origId == tasks[i].origId) {
+			tasks.splice(i, 1);
+		}
+	}
+	
+	showTasks();
+}
+
+/*	completeTask()
+ * 
+ */
+function completeTask(id) {
+	var completing = shown.splice(id, 1);
+	completed.done = true;
+	completed.push(completing);
+	
+	showTasks();
 }
 
 /*	updateTask()
@@ -132,7 +206,27 @@ function editTask(id) {
  * Caled after task editor is closed via "Done" button.
  */
 function updateTask() {
+	var editor = $("#task-editor");
 	
+	if (adding) {
+		var newT = newTask();
+		newT.origId = taskNum;
+		taskNum++;
+		tasks.push(newT);
+		shown.push(newT);
+		var id = shown.length - 1;
+	} else {
+		var id = editor.find("#editing-index").val();
+	}
+	
+	var task = shown[id];
+	
+	task.text = editor.find("#task-text").val();
+	task.place = editor.find("#place").val();
+	task.deadline = editor.find("#deadline").val();
+	task.priority = editor.find("#priority-slider").get(0).value;
+	
+	showTasks();
 }
 
 /*	search()
@@ -153,13 +247,13 @@ function genDemo() {
 	task.text = "Preglej snov za predavanje SP";
 	task.deadline = "24/11/2015";
 	task.place = "Večna Pot 113";
-	task.priority = 5;
+	task.priority = 4;
 	tasks.push(task);
 	task = newTask();
 	task.text = "Rezerviraj karte za fuzbal tekmo";
 	task.deadline = "21/11/2015";
 	task.place = "Arena Stožice";
-	task.priority = 7;
+	task.priority = 5;
 	tasks.push(task);
 	task = newTask();
 	task.text = "Pospravi sobo";
@@ -168,6 +262,8 @@ function genDemo() {
 	tasks.push(task);
 	
 	for (var i in tasks) {
+		tasks[i].origId = i;
 		shown.push(tasks[i]);
+		taskNum++;
 	}
 }
